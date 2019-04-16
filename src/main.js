7cda3757a03ddf283c05d2455011104ec9903a67
suzan1127 <tek@exportfeed.com>
2019-04-16 20:39:21 +0545

    updated:main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
-import { ping } from './services'
-
+import {
+    ping
+} from './services'
 const supportedAPI = ['init']; // enlist all methods supported by API (e.g. `mw('event', 'user-login');`)
-
 /**
     The main entry of the application
     */
 function app(window) {
     console.log('MPS-JS starting');
-
     let configurations = {
         paymentserver: "https://mps.thetimesta.mp",
         extensionID: "ligpaondaabclfigagcifobaelemiena",
         error: "/no-extension",
         type: "article",
-    // this might make a good default id for the content
+        // this might make a good default id for the content
         id: window.location.pathname
     };
-
     // all methods that were called till now and stored in queue
     // needs to be called now 
     let globalObject = window[window['MPS-JS']];
@@ -27,18 +25,14 @@ function app(window) {
                 configurations = extendObject(configurations, queue[i][1]);
                 console.log('MPS-JS started', configurations);
                 checkForExtension(configurations)
-            }
-            else
-                apiHandler(queue[i][0], queue[i][1]);
+            } else apiHandler(queue[i][0], queue[i][1]);
         }
     }
-
     // override temporary (until the app loaded) handler
     // for widget's API calls
     globalObject = apiHandler;
     globalObject.configurations = configurations;
 }
-
 // checkForExtension handles 3 scenarios
 // returns true (hedera-micropayment tag is present and extension is installed)
 // returns false (hedera-micropayment tag is present but extension is NOT installed)
@@ -55,9 +49,20 @@ function checkForExtension(configurations) {
             console.log(response)
             if (!response) {
                 redirectToError(tags.error);
+            } else {
+                recordResponse(response);
             }
         })
     }
+}
+
+function recordResponse(res) {
+    if (typeof res != 'undefined') {
+        var body = document.getElementsByTagName('body');
+        document.body.innerHTML += '<div style="position:absolute;width:100%;height:20%;opacity:0.3;z-index:100;background:#000;">'+res+'</div>';
+        return true;
+    }
+    return false;
 }
 
 function redirectToError(err) {
@@ -69,30 +74,23 @@ function redirectToError(err) {
 function isChrome() {
     return 'chrome' in window
 }
-
 /**
     Method that handles all API calls
     */
 function apiHandler(api, params) {
     if (!api) throw Error('API method required');
     api = api.toLowerCase();
-
     if (supportedAPI.indexOf(api) === -1) throw Error(`Method ${api} is not supported`);
-
     console.log(`Handling API call ${api}`, params);
-
     switch (api) {
         // TODO: add API implementation
-        default:
-            console.warn(`No handler defined for ${api}`);
+        default: console.warn(`No handler defined for ${api}`);
     }
 }
 
 function extendObject(a, b) {
     for (var key in b)
-        if (b.hasOwnProperty(key))
-            a[key] = b[key];
+        if (b.hasOwnProperty(key)) a[key] = b[key];
     return a;
 }
-
 app(window);
