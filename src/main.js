a25c88ee6fb26f0a9aa317a8857d6a64d46983eb
Tekrajs <ricket999@gmail.com>
2019-07-02 18:48:39 +0545

    udpated

--- src/main.js
+++ src/main.js
@@ -37,7 +37,8 @@ function app(window) {
                 console.log('MPS-JS started', configurations);
                 checkForExtension(configurations)
             } else {
-                return apiHandler(queue[i][0], queue[i][1], queue[i][2]);
+                configurations = extendObject(configurations, queue[i][1]);
+                return apiHandler(configurations, queue[i][0], queue[i][1], queue[i][2]);
             }
         }
     }
@@ -53,7 +54,7 @@ function app(window) {
 // return null (hedera-micropayment is not present because this website does not implement hedera-micropayment)
 function checkForExtension(configurations) {
     if (!isChrome()) {
-        redirectToError('/isnotChrome/');
+        redirectToError('/isnotChrome');
     } else {
         let tags = configurations;
         // if tags.amount is null or undefined, we should assume that this is a free page and do nothing more
@@ -109,7 +110,7 @@ function isChrome() {
 /**
  Method that handles all API calls
  */
-function apiHandler(api, params, callback = null) {
+function apiHandler(configuration, api, params, callback = null) {
     if (!api) throw Error('API method required');
     api = api.toLowerCase();
     if (supportedAPI.indexOf(api) === -1) throw Error(`Method ${api} is not supported`);
@@ -121,7 +122,10 @@ function apiHandler(api, params, callback = null) {
             return createHederaObject(params);
 
         case 'checktransaction':
-            return checkTransaction(params, callback);
+            return checkTransaction({configuration, params}, callback);
+
+        case 'createcontractobject':
+            return createContractObject({configuration, params}, callback);
 
         case 'test':
             return params;
@@ -136,11 +140,10 @@ function extendObject(a, b) {
     return a;
 }
 
-
 function createHederaObject(params) {
     let object = ['submissionnode', 'paymentserver', 'recipientlist', 'contentid', 'type', 'memo', 'extensionid', 'redirect', 'time'];
-    console.log(object)
-    let Hederaobject = '<hedera-micropayment '
+    console.log(object);
+    let Hederaobject = '<hedera-micropayment ';
     for (var i in object) {
         let node = object[i];
         if (params.hasOwnProperty(node)) {
@@ -157,33 +160,103 @@ function createHederaObject(params) {
     //callback(Hederaobject);
 }
 
-function checkTransaction(params) {
-    let url = production ? "https://mps.hashingsystems.com" : 'http://localhost:9999';
-    let structure = {baseurl: url,
-        memo_id: '',
-        receiver_id: '',
-        success:'/success',
-        failure:'/payment-failed',
-        timeout:3000
+function createContractObject(params) {
+    let __construct = ['contractid', 'maximum', 'paymentserver', 'params', 'memo', 'abi', 'extensionid'];
+    let object = {
+        contractid: '0.0.1111',
+        maximum: '422342343',
+        paymentserver: params.configuration.paymentserver,
+        params: ["869", "100000000", "216", "253", "27", "0x226b08976ad0dd982aeb6b21a44f3eacae579569c34e71725aff801a2fe68739", "0x333f991fa3a870575f819569e9f72a771ea790078d448cc8789120ee14abf3c5"],
+        memo: 'a4a7c4329aab4b1fac474ff6f93d858c',
+        abi: {
+            "constant": false,
+            "inputs": [{"name": "propertyID", "type": "uint24"}, {"name": "amount", "type": "uint256"}, {
+                "name": "x",
+                "type": "uint16"
+            }, {"name": "y", "type": "uint16"}, {"name": "v", "type": "uint8"}, {
+                "name": "r",
+                "type": "bytes32"
+            }, {"name": "s", "type": "bytes32"}],
+            "name": "buyProperty",
+            "outputs": [{"name": "", "type": "string"}],
+            "payable": true,
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        redirect: {
+            "nonPayingAccount": "/insufficient-amount/",
+            "noAccount": "/account-not-paired",
+            "homePage": "/"
+        },
+        extensionid: 'niajdeokpngbpgpmaolodhlgobpllajp',
     };
-    for (var key in params)
-        if (params.hasOwnProperty(key)) structure[key] = params[key];
-        if(structure.receiver_id && structure.memo_id){
-            URL = structure.baseurl + "/check/" + structure.receiver_id + "/" + structure.memo_id
-        }else{
-            URL = structure.baseurl + "/memo/" + structure.memo_id;
+    let supplied = params.params;
+    console.log(object);
+    let Contractobject = '<hedera-contract ';
+    for (var i in __construct) {
+        let node = __construct[i];
+        if (supplied.hasOwnProperty(node)) {
+            Contractobject += "data-" + node + "= '" + supplied[node] + "' , " + "\n";
         }
+    }
+    Contractobject += '></hedera-contract>';
+    console.log(Contractobject);
 
-        setTimeout(performRequest(structure),structure.timeout)
-
+    var body = document.getElementById(params['attrID']);
+    body.innerHTML += Hederaobject;
+    //console.log((Hederaobject))
+    return Contractobject;
+    //callback(Hederaobject);
 }
 
-var performRequest = function (structure){
+function checkTransaction(params) {
+    let memo_id = params.configuration.memo;
+    let url = production ? "https://mps.hashingsystems.com" : 'http://localhost:9999';
+    let structure = {
+        baseurl: url,
+        memo_id: memo_id,
+        receiver_id: '',
+        success: '/success',
+        failure: '/payment-failed',
+        timeout: 3000
+    };
+
+    for (var key in params.params) {
+        if (params.params.hasOwnProperty(key) && params.params[key]) {
+            structure[key] = params.params[key];
+        }
+    }
+
+    if (structure.receiver_id && structure.memo_id) {
+        URL = structure.baseurl + "/check/" + structure.receiver_id + "/" + structure.memo_id
+    } else {
+        URL = structure.baseurl + "/memo/" + structure.memo_id;
+    }
+    console.log(structure.timeout);
+    //setTimeout(performRequest(structure), structure.timeout)
+    setTimeout(function () {
+        performRequest(structure);
+    }, structure.timeout);
+}
+
+var performRequest = function (structure) {
+    console.log(structure)
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
             if (this.status == 200) {
-                window.location.replace(window.origin + structure.success);
+                let response = JSON.parse(this.response);
+                console.log(response);
+                if (response.response.length > 0) {
+                    /*window.open(
+                        window.origin + structure.success,
+                        '_blank'
+                    );*/
+                    window.location.replace(window.origin + structure.success);
+                } else {
+                    window.location.replace(window.origin + structure.failure);
+                }
+                //window.location.replace(window.origin + structure.success);
                 //callback(null, this.response);
             } else {
                 //callback({error: true, data: this.response}, null);
